3d9ab758bf46eba9e51e0212b877b2666dd0aa25
Оля Бездудная <lyolya0606@gmail.com>
2022-12-02 21:03:25 +0300

    plak

--- Server/MainForm.Designer.cs
+++ Server/MainForm.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace Server {
-    partial class Form1 {
+    partial class MainForm {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -23,6 +23,7 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -33,7 +34,7 @@
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox1.Location = new System.Drawing.Point(32, 36);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(281, 27);
+            this.textBox1.Size = new System.Drawing.Size(221, 27);
             this.textBox1.TabIndex = 0;
             // 
             // button1
@@ -41,26 +42,27 @@
             this.button1.BackColor = System.Drawing.Color.Pink;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(32, 94);
+            this.button1.Location = new System.Drawing.Point(285, 83);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 39);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Connect";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LavenderBlush;
-            this.ClientSize = new System.Drawing.Size(463, 171);
+            this.ClientSize = new System.Drawing.Size(399, 138);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Server";
             this.ResumeLayout(false);
             this.PerformLayout();
 
---
+++ Server/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using LibraryWithLRU_NRU;
+
+namespace Server {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+        bool isOpen = false;
+
+        private void button1_Click(object sender, EventArgs e) {
+            var serviceAddress = "127.0.0.1:10000";
+            var serviceName = "MyService";
+
+
+            // ServiceHost - Реализует узел, используемый моделью программирования модели Windows Communication Foundation (WCF).
+            // Используется для настройки и предоставления службы для использования клиентскими приложениями
+            var host = new ServiceHost(typeof(LRU_NRU), new Uri($"net.tcp://{serviceAddress}/{serviceName}"));
+            if (isOpen) {
+                return;
+            }
+            // NetTcpBinding - Задает безопасную, надежную и оптимизированную привязку, пригодную для обмена данными между компьютерами. 
+            var serverBinding = new NetTcpBinding();
+            // AddServiceEndpoint - При использовании метода узел службы выполняет проверку по имени конфигурации в описании контракта.
+            host.AddServiceEndpoint(typeof(IAlgorithm), serverBinding, "");
+
+           
+
+            host.Open();
+            textBox1.Text = "Host has started";
+            isOpen = true;
+        }
+    }
+}
